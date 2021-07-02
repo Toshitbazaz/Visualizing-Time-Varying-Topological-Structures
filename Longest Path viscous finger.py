e169878b454babc265c6bea3f34508c9b1fe92d5
Toshitbazaz <68945257+Toshitbazaz@users.noreply.github.com>
2021-07-02 12:02:05 +0530

    Update Longest Path viscous finger.py

--- Longest Path viscous finger.py
+++ Longest Path viscous finger.py
@@ -1,3 +1,10 @@
+from paraview.simple import *
+import vtk
+view = GetActiveViewOrCreate('RenderView')
+layout1 = GetLayout()
+layout1.SplitHorizontal(0, 0.5)
+view1 = CreateView('RenderView')
+AssignViewToLayout(view=view1, layout=layout1, hint=2)
 Name = []
 for i in range(13,48):
  Name.append('New\Correspondence\sample1_'+ str(i) +'.vtk')
